3d3ff573ad602855341073370ef4bc15611e6768
Esmaelhbr <esmael7@live.com>
2021-10-07 12:31:07 -0400

    intalled and used yargs with npm

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
 //import 
-const getNote = require("./notes.js");
+
+const yargs= require('yargs');
 const chalk = require("chalk");
+const getNote = require("./notes.js");
+
+//customized yargs version 
+yargs.version('1.1.0')
+
+//add, remove, read, list
+//create add command
+yargs.command({
+	command : 'add',
+	describe: 'Add a new note',
+	handler: function(){
+		console.log("adding a new note")
+	}
+})
+//create remove command
+yargs.command({
+	command: 'remove',
+	describe: 'Remove a note',
+	handler: function(){
+		console.log("removing the note")
+	}
+})
+//create a list command
+yargs.command({
+	command: 'list',
+	describe: 'lists notes',
+	handler: function(){
+		console.log("displaying note list")
+	}
+})
+//create a read command
+yargs.command({
+	command: 'read',
+	describe: 'Read notes',
+	handler: function(){
+		console.log("read a note");
+	}
+})
 
 
-console.log(chalk.green.bold('success'));
-
-const msg= getNote();
-
+console.log(yargs.argv);
